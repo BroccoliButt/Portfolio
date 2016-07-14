0b0b81e2fb877b88d2ebd97aca727697b777d189
Leslie Ruckman <leslieruckman@yahoo.com>
2016-07-14 11:11:59 -0500

    added hr between tumblr posts

--- assets/js/tumblr.js
+++ assets/js/tumblr.js
@@ -8,7 +8,7 @@ var main = function() {
 	/* Show date numbers only */
 		var dateFull = thisPost["date-gmt"];
 		var date = "<div class='date'>" + dateFull.slice(5,7) + " . " + dateFull.slice(8,10) + " . " + dateFull.slice(2,4) + "</div>";
-		var postCaption = "<div class='post-caption col-md-6 col-md-offset-3 col-sm-8 col-sm-offset-2'>" + thisPost["photo-caption"] + "</div>"
+		var postCaption = "<div class='post-caption col-md-6 col-md-offset-3 col-sm-8 col-sm-offset-2'>" + thisPost["photo-caption"] + "<hr>" + "</div>"
 
 	/* Show post photo + date + caption */
 	    $('#tumblog').append("<img src='" + thisPost["photo-url-1280"] + "'>" + date + postCaption);
